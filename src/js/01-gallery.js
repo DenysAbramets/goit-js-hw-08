7e68f386050f8be8c726c9300916e86b1fcbb118
DenysAbramets <abramets_denys@outlook.com>
2023-03-09 15:57:44 +0100

    sdad

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,34 @@
 import { galleryItems } from './gallery-items';
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 
 
 console.log(galleryItems);
 
+const containerGallery = document.querySelector(".gallery");
+ const makeGallery = createGalleryImage(galleryItems);
+
+ containerGallery.innerHTML = makeGallery;
+
+function createGalleryImage(galleryItems){
+    return galleryItems.map(image => {
+        return ` <a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" title="" />
+</a>
+        `
+    }).join("");
+
+};
+
+    
 
 
 
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionPosition: 'bottom',
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
