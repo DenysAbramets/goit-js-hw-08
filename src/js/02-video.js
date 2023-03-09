9152146f6da76143751b5b67d59d23487842637f
DenysAbramets <abramets_denys@outlook.com>
2023-03-09 21:16:06 +0100

    100$

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+const LOCALE_STORAGE = 'videoplayer-current-time'
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const time = player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem(LOCALE_STORAGE, JSON.stringify(data));
+}, 1000));
+
+const array = JSON.parse(localStorage.getItem(LOCALE_STORAGE));
+const currentTime = array['seconds'];
+
+player.setCurrentTime(currentTime);
